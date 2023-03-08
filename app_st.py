756ac683f36155ce0f84630dfb3bbe3f6b2d5097
Darlon <darlonv@gmail.com>
2023-03-08 12:36:25 -0300

    correcao

--- app_st.py
+++ app_st.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.title("Dockertest v 2.0")
 
 val = st.text_input('Valor')
-val = int(val)
-quad = val**2
-st.write(f'Quadrado: {quad}')
+if val:
+    val = int(val)
+    quad = val**2
+    st.write(f'Quadrado: {quad}')
 
